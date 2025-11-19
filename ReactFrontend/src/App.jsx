6c523ac85e63095e519188abf004a90aad7963b5
asajjadbp <147432350+asajjadbp@users.noreply.github.com>
2025-11-19 14:36:09 +0500

    git commit

--- ReactFrontend/src/App.jsx
+++ ReactFrontend/src/App.jsx
@@ -1,12 +1,29 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-
+const [set,getset]=useState(0);
+function Click(){
+  getset(set+1);
+  return
+}
+function Minus(){
+  if(set>0){
+getset(set-1);
+  }
+  
+  return
+}
   return (
     <>
    <h1>hello</h1>
+   {set}
+  {/* {
+  set ==0 ? <button onClick={()=>(getset(set+1))}>+</button> :   <button onClick={()=>(getset(set-1))}>-</button>
+  } */}
+   <button onClick={Click}>+</button>
+   <button onClick={Minus}>-</button>
     </>
   )
 }
